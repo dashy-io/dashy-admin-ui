82459a6f23e2066e7e7b0faa89c777c5fbecc08e
Mattia Asti <mattia.asti@gmail.com>
2014-11-28 15:36:51 +0000

    added loading function and show server online/offline

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,7 @@
         $(e.target).addClass('active'); // activated list-item
     });
 
-    var dashyAdmin = angular.module('dashyAdmin', []);
+    var dashyAdmin = angular.module('dashyAdmin', ['ui.bootstrap']);
 
     dashyAdmin.factory('api', ['$http', function($http) {
         return {
@@ -17,10 +17,17 @@
                 // temporary ID
                 var temp = userID;
                 temp = 'cc1f2ba3-1a19-44f2-ae78-dc9784a2a60f';
-                return $http.get('http://api.dashy.io/user/' + temp);
+                return $http.get('http://api.dashy.io/users/' + temp);
             },
             getDashboard: function(dashboardID) {
                 return $http.get('http://api.dashy.io/dashboards/' + dashboardID);
+            },
+            setDashboard: function(dashboardID, interval, name, urls) {
+                return $http.post('http://api.dashy.io/dashboards/' + dashboardID, {
+                    interval: interval,
+                    name: name,
+                    urls: urls
+                });
             }
         };
     }]);
@@ -34,23 +41,34 @@
             } else {
                 $scope.serverStatus = false;
             }
+        }).error(function() {
+            $scope.serverStatus = false;
+            $scope.serverError = 404;
         });
 
     }]);
 
     // retrieve user's dashboards and update them
-    dashyAdmin.controller('userDashboards', ['$scope', 'api', function($scope, api) {
+    dashyAdmin.controller('userDashboards', ['$scope', 'api', '$modal', function($scope, api, $modal) {
+
+        // fetch the dashboards
         var dashboardsIDs = api.getUserDashboards();
-        $scope.dashboards = [];
         dashboardsIDs.success(function(data) {
             data.dashboards.forEach(function(e) {
                 // e is the dashboard ID
                 api.getDashboard(e).success(function(data) {
                     data.id = e;
+                    data.edit = false;
+                    $scope.dashboards = [];
                     $scope.dashboards.push(data);
                 });
             });
+        }).error(function() {
+            $scope.dashboards = []
+            $scope.dashboardsError = 'Couldn\'t load your dashboards';
         });
+
+        // update a dashboard
 
     }]);
 
