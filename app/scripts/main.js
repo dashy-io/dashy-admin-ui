5dea23bad2e071e2a859f43a57ed777719c83b1f
Mattia <mattia.asti@gmail.com>
2014-11-30 23:53:01 +0000

    started to write test and small refactoring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,132 +1,127 @@
 'use strict';
-(function() {
-    $('.list-group-item').on('click', function(e) {
-        var previous = $(this).closest('.list-group').children('.active');
-        previous.removeClass('active'); // previous list-item
-        $(e.target).addClass('active'); // activated list-item
+
+$('.list-group-item').on('click', function(e) {
+    var previous = $(this).closest('.list-group').children('.active');
+    previous.removeClass('active'); // previous list-item
+    $(e.target).addClass('active'); // activated list-item
+});
+
+var dashyAdmin = angular.module('dashyAdmin', ['ui.bootstrap', 'ngRoute']);
+
+// config the routes
+dashyAdmin.config(['$routeProvider', '$locationProvider',
+    function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/dashboards/:dashboardId', {
+                templateUrl: 'dashboard.html',
+                controller: 'DashboardCtrl',
+                controllerAs: 'dashboard'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+]);
+
+// all the API calls are here
+dashyAdmin.factory('api', ['$http', function($http) {
+    return {
+        getServerStatus: function() {
+            return $http.get('http://api.dashy.io/status');
+        },
+        getUserDashboards: function(userId) {
+            // temporary ID
+            var temp = userId;
+            temp = 'cc1f2ba3-1a19-44f2-ae78-dc9784a2a60f';
+            return $http.get('http://api.dashy.io/users/' + temp);
+        },
+        getDashboard: function(dashboardId) {
+            return $http.get('http://api.dashy.io/dashboards/' + dashboardId);
+        },
+        setDashboard: function(dashboard) {
+
+            return $http({
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Methods': 'GET, POST, PUT, OPTIONS',
+                },
+                data: {
+                    'id': dashboard.id,
+                    'interval': dashboard.interval,
+                    'name': dashboard.name,
+                    'urls': dashboard.urls
+                },
+                url: 'http://api.dashy.io/dashboards/' + dashboard.id
+            });
+        }
+    };
+}]);
+
+// check the server status
+dashyAdmin.controller('ServerStatusCtrl', ['$scope', 'api', function($scope, api) {
+    var connected = api.getServerStatus();
+    connected.success(function(data, status) {
+        // should be 200 if it's okay
+        $scope.serverStatus = status;
+    }).error(function() {
+        $scope.serverStatus = 0;
     });
 
-    var dashyAdmin = angular.module('dashyAdmin', ['ui.bootstrap', 'ngRoute']);
+}]);
 
-    // config the routes
-    dashyAdmin.config(['$routeProvider', '$locationProvider',
-        function($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'main.html',
-                    controller: 'MainCtrl',
-                    controllerAs: 'main'
-                })
-                .when('/dashboards/:dashboardId', {
-                    templateUrl: 'dashboard.html',
-                    controller: 'DashboardCtrl',
-                    controllerAs: 'dashboard'
-                })
-                .otherwise({
-                    redirectTo: '/'
-                });
-        }
-    ]);
+// retrieve user's dashboards and update them
+dashyAdmin.controller('MainCtrl', ['$scope', 'api', function($scope, api) {
 
-    // all the API calls are here
-    dashyAdmin.factory('api', ['$http', function($http) {
-        return {
-            getServerStatus: function() {
-                return $http.get('http://api.dashy.io/status');
-            },
-            getUserDashboards: function(userId) {
-                // temporary ID
-                var temp = userId;
-                temp = 'cc1f2ba3-1a19-44f2-ae78-dc9784a2a60f';
-                return $http.get('http://api.dashy.io/users/' + temp);
-            },
-            getDashboard: function(dashboardId) {
-                return $http.get('http://api.dashy.io/dashboards/' + dashboardId);
-            },
-            setDashboard: function(dashboard) {
-
-                return $http({
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Methods': 'GET, POST, PUT, OPTIONS',
-                    },
-                    data: {
-                        'id': dashboard.id,
-                        'interval': dashboard.interval,
-                        'name': dashboard.name,
-                        'urls': dashboard.urls
-                    },
-                    url: 'http://api.dashy.io/dashboards/' + dashboard.id
-                });
-            }
-        };
-    }]);
-
-    // check the server status
-    dashyAdmin.controller('ServerStatusCtrl', ['$scope', 'api', function($scope, api) {
-        var connected = api.getServerStatus();
-        connected.success(function(data, status) {
-            if (status === 200) {
-                $scope.serverStatus = true;
-            } else {
-                $scope.serverStatus = false;
-            }
-        }).error(function() {
-            $scope.serverStatus = false;
-            $scope.serverError = 404;
-        });
-
-    }]);
-
-    // retrieve user's dashboards and update them
-    dashyAdmin.controller('MainCtrl', ['$scope', 'api', function($scope, api) {
-
-        // fetch the dashboards for the current user
-        var dashboardsIds = api.getUserDashboards();
-        dashboardsIds.success(function(data) {
-            data.dashboards.forEach(function(e) {
-                // e is the dashboard ID
-                api.getDashboard(e).success(function(data) {
-                    $scope.dashboards = [];
-                    $scope.dashboards.push(data);
-                });
+    // fetch the dashboards for the current user
+    var dashboardsIds = api.getUserDashboards();
+    dashboardsIds.success(function(data) {
+        data.dashboards.forEach(function(e) {
+            // e is the dashboard ID
+            api.getDashboard(e).success(function(data) {
+                $scope.dashboards = [];
+                $scope.dashboards.push(data);
             });
-        }).error(function() {
-            $scope.dashboards = [];
-            $scope.dashboardsError = 'Couldn\'t load your dashboards';
         });
+    }).error(function() {
+        $scope.dashboards = [];
+        $scope.dashboardsError = 'Couldn\'t load your dashboards';
+    });
 
-    }]);
+}]);
 
-    // view and update a dashboard
-    dashyAdmin.controller('DashboardCtrl', ['$scope', 'api', '$routeParams', function($scope, api, $routeParams) {
+// view and update a dashboard
+dashyAdmin.controller('DashboardCtrl', ['$scope', 'api', '$routeParams', function($scope, api, $routeParams) {
 
-        // fetch the current dashboards
-        api.getDashboard($routeParams.dashboardId).success(function(data) {
-            $scope.dashboard = data;
+    // fetch the current dashboards
+    api.getDashboard($routeParams.dashboardId).success(function(data) {
+        $scope.dashboard = data;
+    });
+
+    // add another url
+    $scope.addUrl = function() {
+        $scope.dashboard.urls.push('');
+    };
+
+    // remove an url
+    $scope.removeUrl = function(url) {
+        $scope.dashboard.urls.splice(url, 1);
+    };
+
+    // update/save a dashboard
+    $scope.saveDashboard = function(dashboard) {
+        console.log(dashboard);
+        api.setDashboard(dashboard).success(function(data, status) {
+            console.log(status);
+        }).error(function(data, status) {
+            // TODO tell the user that there was an error updating
+            console.log('error updating');
         });
+    };
 
-        // add another url
-        $scope.addUrl = function() {
-            $scope.dashboard.urls.push('');
-        };
-
-        // remove an url
-        $scope.removeUrl = function(url) {
-            $scope.dashboard.urls.splice(url, 1);
-        };
-
-        // update/save a dashboard
-        $scope.saveDashboard = function(dashboard) {
-            console.log(dashboard);
-            api.setDashboard(dashboard).success(function(data, status) {
-                console.log(status);
-            }).error(function(data, status) {
-                console.log('error updating:' + status);
-            });
-        };
-
-    }]);
-
-}());
+}]);
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,43 @@
 /* global describe, it */
 
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+    describe('Unit: Testing Controllers', function() {
 
-      });
+        beforeEach(module('dashyAdmin'));
+
+        it('should have a ServerStatusCtrl controller', function() {
+            expect(dashyAdmin.ServerStatusCtrl).not.to.equal(null);
+        });
+
+        it('should have a MainCtrl controller', function() {
+            expect(dashyAdmin.MainCtrl).not.to.equal(null);
+        });
+
+        it('should have a DashboardCtrl controller', function() {
+            expect(dashyAdmin.DashboardCtrl).not.to.equal(null);
+        });
     });
-  });
+
+    describe('Unit: ServerStatusCtrl', function() {
+
+        it('should have a properly working ServerStatus controller', inject(function($rootScope, $controller, $httpBackend) {
+
+
+            var response = $httpBackend.expectJSONP(
+                'http://api.dashy.io/status');
+            response.respond(null);
+
+            var $scope = $rootScope.$new();
+            var ctrl = $controller('ServerStatusCtrl', {
+                $scope: $scope
+            });
+
+            expect($scope.serverStatus).to.be.equal(200);
+
+        }));
+
+    });
+
 })();
