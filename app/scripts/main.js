6af41ff44fb14dac4a7ca8c0366aff3d7be5c975
Mattia <mattia.asti@gmail.com>
2014-12-09 23:06:21 +0000

    created branch with uiRouter instead of angular-route

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,45 @@
 'use strict';
 
-$('.list-group-item').on('click', function(e) {
-    var previous = $(this).closest('.list-group').children('.active');
-    previous.removeClass('active'); // previous list-item
-    $(e.target).addClass('active'); // activated list-item
-});
+// $('.list-group-item').on('click', function(e) {
+//     var previous = $(this).closest('.list-group').children('.active');
+//     previous.removeClass('active'); // previous list-item
+//     $(e.target).addClass('active'); // activated list-item
+// });
 
-var dashyAdmin = angular.module('dashyAdmin', ['ngRoute']);
+var dashyAdmin = angular.module('dashyAdmin', ['ngStorage', 'ui.router']);
 
 // config the routes
-dashyAdmin.config(['$routeProvider', '$locationProvider',
-    function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'main.html',
-                controller: 'MainCtrl',
-                controllerAs: 'main'
-            })
-            .when('/dashboards/:dashboardId', {
-                templateUrl: 'dashboard.html',
-                controller: 'DashboardCtrl',
-                controllerAs: 'dashboard'
-            })
-            .when('/settings', {
-                template: ''
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    }
-]);
+dashyAdmin.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/dashboards');
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            views: {
+                'content': {
+                    templateUrl: 'login.html',
+                    controller: 'LoginCtrl'
+                }
+            }
+        })
+        .state('dashboardsList', {
+            url: '/dashboards',
+            views: {
+                'content': {
+                    templateUrl: 'dashboardList.html',
+                    controller: 'MainCtrl'
+                }
+            }
+        })
+        .state('dashboardEdit', {
+            url: '/dashboards/:dashboardId',
+            views: {
+                'content': {
+                    templateUrl: 'dashboardEdit.html',
+                    controller: 'DashboardCtrl'
+                }
+            }
+        });
+}]);
 
 // all the API calls are here
 dashyAdmin.factory('api', ['$http', function($http) {
@@ -75,16 +85,51 @@ dashyAdmin.factory('api', ['$http', function($http) {
     };
 }]);
 
+dashyAdmin.factory('auth', ['$localStorage', function($localStorage) {
+    return {
+        login: function(user) {
+            $localStorage.dashyUser = user;
+        },
+        logout: function() {
+            delete $localStorage.dashyUser;
+        }
+    };
+}]);
+
+dashyAdmin.factory('checkLogin', ['$localStorage', '$location', function($localStorage, $location) {
+    if ($localStorage.dashyUser) {
+        return true;
+    } else {
+        return false;
+    }
+}]);
+
 // store globally if the server is connected
 dashyAdmin.value('serverConnected', false);
 
 // value to change with auth, now is hard coded
-dashyAdmin.value('currentUser', 'cc1f2ba3-1a19-44f2-ae78-dc9784a2a60f');
+// dashyAdmin.value('currentUser', 'cc1f2ba3-1a19-44f2-ae78-dc9784a2a60f');
 
 // TODO reconnect button in case the server is down at first try
 
+// login controller
+dashyAdmin.controller('LoginCtrl', ['$scope', '$localStorage', function($scope, $localStorage) {
+
+    $scope.user = {
+        username: 'hello',
+        password: ''
+    };
+
+    $scope.$storage = $localStorage;
+
+    $scope.login = function(user) {
+        console.log(user);
+    };
+
+}]);
+
 // check the server status
-dashyAdmin.controller('ServerStatusCtrl', ['$scope', 'api', 'serverConnected', function($scope, api, serverConnected) {
+dashyAdmin.controller('ServerStatusCtrl', ['$scope', 'api', 'serverConnected', 'checkLogin', '$location', function($scope, api, serverConnected, checkLogin, $location) {
 
     var connected = api.getServerStatus();
 
@@ -95,8 +140,14 @@ dashyAdmin.controller('ServerStatusCtrl', ['$scope', 'api', 'serverConnected', f
         // store globally that the server is connected, otherwise don't allow request
         serverConnected = true;
 
-        // enable the button to open the modal to connect a new device
-        $('.btn-newDevice').prop('disabled', false);
+        // check if the user is authenticated or redirect to login page
+        if (!checkLogin) {
+            $location.path('/login');
+        } else {
+
+            // enable the button to open the modal to connect a new device
+            $('.btn-newDevice').prop('disabled', false);
+        }
     }).error(function() {
         $scope.serverStatus = 0;
     });
@@ -104,7 +155,7 @@ dashyAdmin.controller('ServerStatusCtrl', ['$scope', 'api', 'serverConnected', f
 }]);
 
 // check the server status
-dashyAdmin.controller('NewDeviceCtrl', ['$scope', 'api', 'currentUser', '$timeout', function($scope, api, currentUser, $timeout) {
+dashyAdmin.controller('NewDeviceCtrl', ['$scope', 'api', '$timeout', function($scope, api, $timeout) {
 
     $scope.shortCode = null;
     $scope.validateShortCode = null;
@@ -112,7 +163,7 @@ dashyAdmin.controller('NewDeviceCtrl', ['$scope', 'api', 'currentUser', '$timeou
     // TODO finish this
     $scope.newDevice = function() {
 
-        if ($scope.shortCode === null || $scope.shortCode.length !== 6) {
+        if ($scope.shortCode === null || $scope.shortCode.length !== 8) {
 
             $scope.validateShortCode = false;
 
@@ -141,16 +192,20 @@ dashyAdmin.controller('NewDeviceCtrl', ['$scope', 'api', 'currentUser', '$timeou
 }]);
 
 // retrieve user's dashboards and update them
-dashyAdmin.controller('MainCtrl', ['$scope', 'api', 'currentUser', function($scope, api, currentUser) {
+dashyAdmin.controller('MainCtrl', ['$scope', 'api', function($scope, api) {
 
     // fetch the dashboards for the current user
-    var dashboardsIds = api.getUserDashboards(currentUser);
+    var dashboardsIds = api.getUserDashboards($localStorage.dashyUser.username);
     dashboardsIds.success(function(data) {
         data.dashboards.forEach(function(e) {
             // e is the dashboard ID
             api.getDashboard(e).success(function(data) {
                 $scope.dashboards = [];
                 $scope.dashboards.push(data);
+            }).error(function(data) {
+                $.snackbar({
+                    content: '<i class="fa fa-3x fa-ban pull-left"></i>' + e + '<br>' + data.message
+                });
             });
         });
     }).error(function() {
@@ -168,10 +223,10 @@ dashyAdmin.controller('MainCtrl', ['$scope', 'api', 'currentUser', function($sco
 }]);
 
 // view and update a dashboard
-dashyAdmin.controller('DashboardCtrl', ['$scope', 'api', '$routeParams', '$timeout', function($scope, api, $routeParams, $timeout) {
+dashyAdmin.controller('DashboardCtrl', ['$scope', 'api', '$stateParams', function($scope, api, $stateParams) {
 
     // fetch the current dashboards
-    api.getDashboard($routeParams.dashboardId).success(function(data) {
+    api.getDashboard($stateParams.dashboardId).success(function(data) {
         $scope.dashboard = data;
     });
 
@@ -190,10 +245,10 @@ dashyAdmin.controller('DashboardCtrl', ['$scope', 'api', '$routeParams', '$timeo
         $('.btn-save').button('loading');
         $('.btn-save').prop('disabled', true);
         api.setDashboard(dashboard).success(function() {
-            $('.btn-save').button('complete');
-            $timeout(function() {
-                $('.btn-save').button('reset');
-            }, 1500);
+            $.snackbar({
+                content: 'Your dashboard has been updated!'
+            });
+            $('.btn-save').button('reset');
         }).error(function(error) {
             // TODO tell the user that there was an error updating
             window.alert('error updating: ' + error);
