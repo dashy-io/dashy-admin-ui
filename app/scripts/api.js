38a4c5bc32da282557c9fd6fb6c9c67280289c2a
Mattia <mattia.asti@gmail.com>
2015-03-01 00:03:52 +0000

    New deploy with rsync

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -3,64 +3,64 @@
 var baseUrl = 'http://api.dashy.io';
 
 angular.module('dashyAdmin').factory('Api', ['$http',
-  function($http) {
-    return {
-      getServerStatus: function() {
-        return $http.get(baseUrl + '/status');
-      },
-      getUserDashboards: function(userId) {
-        return $http.get(baseUrl + '/users/' + userId);
-      },
-      claimDashboard: function(userId, code) {
-        return $http({
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          data: {
-            code: code
-          },
-          url: baseUrl + '/users/' + userId + '/dashboards'
-        });
-      },
-      getDashboard: function(dashboardId) {
-        return $http.get(baseUrl + '/dashboards/' + dashboardId);
-      },
-      deleteDashboard: function(dashboardId) {
-        return $http.delete(baseUrl + '/dashboards/' + dashboardId);
-      },
-      disconnectDashboard: function(userId, dashboardId) {
-        return $http.delete(baseUrl + '/users/' + userId +  '/dashboards/' + dashboardId);
-      },
-      setDashboard: function(dashboard) {
-        return $http({
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          data: {
-            'interval': dashboard.interval,
-            'name': dashboard.name,
-            'urls': dashboard.urls
-          },
-          url: baseUrl + '/dashboards/' + dashboard.id
-        });
-      },
-      authenticateGoogleUser: function(token) {
-        return $http.post(baseUrl + '/auth/google/login', {
-          /*jshint camelcase: false */
-          access_token: token.access_token
-        });
-      },
-      getUser: function() {
-        return $http.get(baseUrl + '/user');
-      },
-      signupGoogleUser: function(token) {
-        return $http.post(baseUrl + '/auth/google/signup', {
-          /*jshint camelcase: false */
-          access_token: token.access_token
-        });
-      }
-    };
-  }
+    function($http) {
+        return {
+            getServerStatus: function() {
+                return $http.get(baseUrl + '/status');
+            },
+            getUserDashboards: function(userId) {
+                return $http.get(baseUrl + '/users/' + userId);
+            },
+            claimDashboard: function(userId, code) {
+                return $http({
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        code: code
+                    },
+                    url: baseUrl + '/users/' + userId + '/dashboards'
+                });
+            },
+            getDashboard: function(dashboardId) {
+                return $http.get(baseUrl + '/dashboards/' + dashboardId);
+            },
+            deleteDashboard: function(dashboardId) {
+                return $http.delete(baseUrl + '/dashboards/' + dashboardId);
+            },
+            disconnectDashboard: function(userId, dashboardId) {
+                return $http.delete(baseUrl + '/users/' + userId + '/dashboards/' + dashboardId);
+            },
+            setDashboard: function(dashboard) {
+                return $http({
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        'interval': dashboard.interval,
+                        'name': dashboard.name,
+                        'urls': dashboard.urls
+                    },
+                    url: baseUrl + '/dashboards/' + dashboard.id
+                });
+            },
+            authenticateGoogleUser: function(token) {
+                return $http.post(baseUrl + '/auth/google/login', {
+                    /*jshint camelcase: false */
+                    access_token: token.access_token
+                });
+            },
+            getUser: function() {
+                return $http.get(baseUrl + '/user');
+            },
+            signupGoogleUser: function(token) {
+                return $http.post(baseUrl + '/auth/google/signup', {
+                    /*jshint camelcase: false */
+                    access_token: token.access_token
+                });
+            }
+        };
+    }
 ]);
--- gulpfile.js
+++ gulpfile.js
@@ -2,142 +2,143 @@
 'use strict';
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var deploy = require('gulp-gh-pages');
 var runSequence = require('run-sequence');
 
 gulp.task('styles', function() {
-  return gulp.src('app/styles/main.css')
-    .pipe($.autoprefixer({
-      browsers: ['last 1 version']
-    }))
-    .pipe(gulp.dest('.tmp/styles'));
+    return gulp.src('app/styles/main.css')
+        .pipe($.autoprefixer({
+            browsers: ['last 1 version']
+        }))
+        .pipe(gulp.dest('.tmp/styles'));
 });
 
 gulp.task('jshint', function() {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'));
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.jshint.reporter('fail'));
 });
 
 gulp.task('html', ['styles'], function() {
-  var assets = $.useref.assets({
-    searchPath: '{.tmp,app}'
-  });
+    var assets = $.useref.assets({
+        searchPath: '{.tmp,app}'
+    });
 
-  return gulp.src('app/**/*.html')
-    .pipe(assets)
-    //.pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.csso()))
-    .pipe($.rev())
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe($.if('**/*.html', $.minifyHtml({
-      conditionals: true,
-      loose: true,
-      empty: true
-    })))
-    .pipe($.revReplace())
-    .pipe(gulp.dest('dist'));
+    return gulp.src('app/**/*.html')
+        .pipe(assets)
+        //.pipe($.if('*.js', $.uglify()))
+        .pipe($.if('*.css', $.csso()))
+        .pipe($.rev())
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe($.if('**/*.html', $.minifyHtml({
+            conditionals: true,
+            loose: true,
+            empty: true
+        })))
+        .pipe($.revReplace())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('images', function() {
-  return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
-    .pipe(gulp.dest('dist/images'));
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('fonts', function() {
-  return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
-    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-    .pipe($.flatten())
-    .pipe(gulp.dest('dist/fonts'));
+    return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
+        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('extras', function() {
-  return gulp.src([
-    'app/*.*',
-    '!app/*.html',
-    'app/CNAME'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist'));
+    return gulp.src([
+        'app/*.*',
+        '!app/*.html',
+        'app/CNAME'
+    ], {
+        dot: true
+    }).pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
 gulp.task('connect', function() {
-  var serveStatic = require('serve-static');
-  var serveIndex = require('serve-index');
-  var app = require('connect')()
-    .use(require('connect-livereload')({
-      port: 35729
-    }))
-    .use(serveStatic('.tmp'))
-    .use(serveStatic('app'))
-    // paths to bower_components should be relative to the current file
-    // e.g. in app/index.html you should use ../bower_components
-    .use('/bower_components', serveStatic('bower_components'))
-    .use(serveIndex('app'));
+    var serveStatic = require('serve-static');
+    var serveIndex = require('serve-index');
+    var app = require('connect')()
+        .use(require('connect-livereload')({
+            port: 35729
+        }))
+        .use(serveStatic('.tmp'))
+        .use(serveStatic('app'))
+        // paths to bower_components should be relative to the current file
+        // e.g. in app/index.html you should use ../bower_components
+        .use('/bower_components', serveStatic('bower_components'))
+        .use(serveIndex('app'));
 
-  require('http').createServer(app)
-    .listen(9000)
-    .on('listening', function() {
-      console.log('Started connect web server on http://localhost:9000');
-    });
+    require('http').createServer(app)
+        .listen(9000)
+        .on('listening', function() {
+            console.log('Started connect web server on http://localhost:9000');
+        });
 });
 
 gulp.task('serve', ['connect', 'watch'], function() {
-  require('opn')('http://localhost:9000');
+    require('opn')('http://localhost:9000');
 });
 
 // inject bower components
 gulp.task('wiredep', function() {
-  var wiredep = require('wiredep').stream;
-  gulp.src('app/*.html')
-    .pipe(wiredep({
-      directory: 'bower_components'
-    }))
-    .pipe(gulp.dest('app'));
+    var wiredep = require('wiredep').stream;
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'bower_components'
+        }))
+        .pipe(gulp.dest('app'));
 });
 
 gulp.task('watch', ['connect'], function() {
-  $.livereload.listen();
+    $.livereload.listen();
 
-  // watch for changes
-  gulp.watch([
-    'app/*.html',
-    'app/views/*.html',
-    '.tmp/styles/**/*.css',
-    'app/scripts/**/*.js',
-    'app/images/**/*'
-  ]).on('change', $.livereload.changed);
+    // watch for changes
+    gulp.watch([
+        'app/*.html',
+        'app/views/*.html',
+        '.tmp/styles/**/*.css',
+        'app/scripts/**/*.js',
+        'app/images/**/*'
+    ]).on('change', $.livereload.changed);
 
-  gulp.watch('app/styles/**/*.css', ['styles']);
-  gulp.watch('bower.json', ['wiredep']);
+    gulp.watch('app/styles/**/*.css', ['styles']);
+    gulp.watch('bower.json', ['wiredep']);
 });
 
 gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function() {
-  return gulp.src('dist/**/*').pipe($.size({
-    title: 'build',
-    gzip: true
-  }));
-});
-
-gulp.task('default', ['clean'], function() {
-  gulp.start('build');
-});
-
-gulp.task('gh-pages', function() {
-  return gulp.src(['dist/**/*.*', 'dist/CNAME'])
-    .pipe(deploy({
-      cacheDir: '.tmp/dist/'
+    return gulp.src('dist/**/*').pipe($.size({
+        title: 'build',
+        gzip: true
     }));
 });
 
+gulp.task('default', ['clean'], function() {
+    gulp.start('build');
+});
+
+gulp.task('rsync', function() {
+    return gulp.src('dist/**/*.*')
+        .pipe($.rsync({
+            username: 'deploy',
+            hostname: '37.139.31.35',
+            destination: '/var/www/dashy.io/admin/'
+        }));
+});
+
 gulp.task('deploy', function() {
-  runSequence('clean','build','gh-pages');
+    runSequence('clean', 'build', 'rsync');
 });
